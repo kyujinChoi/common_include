24c7233384611544c0c080abdb476c74ee70ea63
qjinchoi <qjinchoi@gmail.com>
2025-01-31 15:39:15 +0900

    header included

--- utils/string.h
+++ utils/string.h
@@ -5,24 +5,27 @@
 #include <cstring>
 #include <vector>
 #include <sstream>
-inline std::string to_string_with_precision(double value, int precision = 2) {
+#include <iomanip>
+inline std::string to_string_with_precision(double value, int precision = 2)
+{
     std::ostringstream out;
     out << std::fixed << std::setprecision(precision) << value;
     return out.str();
 }
-inline std::vector<std::string> split(std::string str, char Delimiter) 
+inline std::vector<std::string> split(std::string str, char Delimiter)
 {
-    std::istringstream iss(str);             // istringstream에 str을 담는다.
-    std::string buffer;                      // 구분자를 기준으로 절삭된 문자열이 담겨지는 버퍼
- 
+    std::istringstream iss(str); // istringstream에 str을 담는다.
+    std::string buffer;          // 구분자를 기준으로 절삭된 문자열이 담겨지는 버퍼
+
     std::vector<std::string> result;
- 
+
     // istringstream은 istream을 상속받으므로 getline을 사용할 수 있다.
-    while (getline(iss, buffer, Delimiter)) {
+    while (getline(iss, buffer, Delimiter))
+    {
         buffer.erase(remove(buffer.begin(), buffer.end(), ' '), buffer.end());
-        result.push_back(buffer);               // 절삭된 문자열을 vector에 저장
+        result.push_back(buffer); // 절삭된 문자열을 vector에 저장
     }
- 
+
     return result;
 }
 inline std::string replaceAll(std::string str, std::string from_delimiter, std::string to_delimiter)
@@ -41,7 +44,7 @@ inline bool isContain(std::string source_str, std::string sub_str)
     std::string::size_type n = source_str.find(sub_str);
     if (n == std::string::npos)
         return false;
-    else 
+    else
         return true;
 }
 #endif
